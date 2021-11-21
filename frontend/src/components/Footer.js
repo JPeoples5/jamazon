8e93724eb8681e286ff246ad9bde9c61366cbf39
Jeremiah Peoples <jeremiah@Jeremiahs-MBP.local>
2021-11-21 12:10:22 -0600

    First commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Container, Row, Col } from "react-bootstrap"
+
+export const Footer = () => {
+	return (
+		<Container>
+			<Row>
+				<Col className='text-center py-3'>Copyright &copy; Jamazon</Col>
+			</Row>
+		</Container>
+	)
+}
